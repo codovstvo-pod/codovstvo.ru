9ac8901425d6bb019d12b75975198951d302db1a
keshoxvol <keshoxvol@gmail.com>
2023-12-05 13:04:04 +0300

    версия

--- games/globalwar/index.06a00.js
+++ games/globalwar/index.06a00.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.c2bf3.js"], function (_export, _context) {
   "use strict";
 
   var Application, application;
@@ -8,8 +8,8 @@ System.register(["./application.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationJs) {
-      Application = _applicationJs.Application;
+    setters: [function (_applicationC2bf3Js) {
+      Application = _applicationC2bf3Js.Application;
     }],
     execute: function () {
       application = new Application();
