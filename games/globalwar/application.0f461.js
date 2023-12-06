8c9be436f6e6dd3687cff492c851bda8d26dcc59
keshoxvol <keshoxvol@gmail.com>
2023-12-06 15:00:25 +0300

    build

--- games/globalwar/application.0f461.js
+++ games/globalwar/application.0f461.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.1750f.json';
+          this.settingsPath = 'src/settings.215a6.json';
           this.showFPS = false;
         }
 
--- games/globalwar/index.70890.js
+++ games/globalwar/index.70890.js
@@ -1,4 +1,4 @@
-System.register(["./application.c2bf3.js"], function (_export, _context) {
+System.register(["./application.0f461.js"], function (_export, _context) {
   "use strict";
 
   var Application, application;
@@ -8,8 +8,8 @@ System.register(["./application.c2bf3.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationC2bf3Js) {
-      Application = _applicationC2bf3Js.Application;
+    setters: [function (_application0f461Js) {
+      Application = _application0f461Js.Application;
     }],
     execute: function () {
       application = new Application();
